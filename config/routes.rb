37afe5945418a15e2b5eafd589082223a6b5974e
Alessandro Mura <alessandro.mura@futurelearn.com>
2017-11-27 12:46:53 +0000

    Add application form for requesting to enrol on a degree or degree program
    This commit introduces a simple application form at `/apply` that uses the
    various FutureLearn Partners API endpoints to prefill form fields and display
    the title of the degree or program the student is applying to join. This is all
    facilitated by the `token` query string param that is provided when the student
    is redirected from FutureLearn, which happens at the beginning of the
    FutureLearn-first enrolment flow.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/apply', to: 'enrolment_requests#new'
+  post '/apply', to: 'enrolment_requests#create'
+
 end
